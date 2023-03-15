d50c03c9a67811957973566f7b5e1ce77f184f74
duongtd <duong.tranduc@ncc.asia>
2023-03-15 23:48:19 +0700

    add api review user

---
+++ 8.0.0/aspnet-core/src/Proman.Application/APIs/ReviewUsers/Dto/ReviewUserCreateEditDto.cs
@@ -0,0 +1,28 @@
+﻿using Abp.AutoMapper;
+using Abp.Domain.Entities;
+using Proman.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Proman.Constants.Enum.StatusEnum;
+
+namespace Proman.APIs.ReviewUsers.Dto
+{
+    [AutoMapTo(typeof(ReviewUser))]
+    public class ReviewUserCreateEditDto : Entity<long>
+    {
+        [Required]
+        public string Name { get; set; }
+        public ReviewUserStatus Status { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public DateTime EndDate { get; set; }
+        [Required]
+        
+        public DateTime Deadline { get; set; }
+    }
+}
---
+++ 8.0.0/aspnet-core/src/Proman.Application/APIs/ReviewUsers/Dto/ReviewUserDto.cs
@@ -0,0 +1,21 @@
+﻿using Abp.Domain.Entities;
+using Proman.Anotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Proman.Constants.Enum.StatusEnum;
+
+namespace Proman.APIs.ReviewUsers.Dto
+{
+    public class ReviewUserDto : Entity<long>
+    {
+        [ApplySearch]
+        public string Name { get; set; }
+        public ReviewUserStatus Status { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime Deadline { get; set; }
+    }
+}
